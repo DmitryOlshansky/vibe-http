94bf38e3b471edcd450bcbb25e4c6e18177f35d2
Dmitry Olshansky <dmitry@olshansky.me>
2025-10-17 17:06:13 +0300

    Latest version with proper runWorkerTaskDist

--- examples/http_server/source/app.d
+++ examples/http_server/source/app.d
@@ -1,20 +1,25 @@
-import vibe.core.core : runApplication;
+import vibe.core.core;
 import vibe.http.server;
 
 void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res)
 {
-	if (req.path == "/")
+	if (req.path == "/") {
 		res.writeBody("Hello, World!", "text/plain");
+	}
 }
 
 void main()
 {
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-
-	auto l = listenHTTP(settings, &handleRequest);
-	scope (exit) l.stopListening();
-
+	runWorkerTaskDist(() nothrow {
+		try {
+			auto settings = new HTTPServerSettings;
+			settings.port = 8080;
+			settings.options = HTTPServerOption.reusePort;
+			settings.bindAddresses = ["::1", "127.0.0.1"];
+			auto l = listenHTTP(settings, &handleRequest);
+		} catch(Exception e) {
+			assert(false, e.msg);
+		}
+	});
 	runApplication();
 }
